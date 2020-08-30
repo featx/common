0ece7fe5058178c3c3c4f03565955c6c31e164b1
之外 <excepts@featx.org>
2020-08-30 14:02:19 +0800

    Start optional util

---
+++ src/main/java/org/featx/spec/util/OptionalUtil.java
@@ -0,0 +1,31 @@
+package org.featx.spec.util;
+
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+/**
+ * @author Excepts
+ * @since 2020/8/30 13:47
+ */
+public class OptionalUtil {
+    private OptionalUtil() {
+
+    }
+
+    public static <O, R> R nullDefault(O o, Function<? super O, R> function, R d4tR) {
+        return Optional.ofNullable(o).map(function).orElse(d4tR);
+    }
+
+    public static <O, R> R nullDefault(O o, Function<? super O, R> function, Supplier<R> rSupplier) {
+        return Optional.ofNullable(o).map(function).orElseGet(rSupplier);
+    }
+
+    public static <O> boolean nullTrue(O o, Function<? super O, Boolean> function) {
+        return nullDefault(o, function, true);
+    }
+
+    public static <O> boolean nullFalse(O o, Function<? super O, Boolean> function) {
+        return nullDefault(o, function, true);
+    }
+}
